583a25a187541e3615a3c36f32dd2eff879d3e2e
medo50000 <45098450+medo50000@users.noreply.github.com>
2019-01-08 01:25:47 +0200

    Add files via upload

---
+++ bot (4).js
@@ -0,0 +1,13 @@
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('-bc-users')){
+if(!message.author.id === '468479699543130132') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+client.login('حط هنا التوكن ياستا');
